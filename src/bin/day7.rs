df540c988359ab8acc696fcf50e8b0b89ae8eb24
Ruleofnine <ruleofnine@outlook.com>
2022-12-07 19:48:20 -0500

    working on day 7 still

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,6 +1,6 @@
 const EXAMPLE_INPUT: &str = "$ cd /
 $ ls
-dir a
+dir e
 14848514 b.txt
 8504156 c.dat
 dir d
@@ -22,7 +22,8 @@ $ ls
 5626152 d.ext
 7214296 k";
 fn part_one(input: String) -> i32 {
-    let mut dirs = std::collections::HashMap::new();
+    let mut dirs: std::collections::HashMap<String, i32> = std::collections::HashMap::new();
+    let mut cdir: &str = "/";
     let mut pwd = vec![];
     input.lines().for_each(|line| {
         if let Some(dir) = line.strip_prefix("$ cd ") {
@@ -30,36 +31,36 @@ fn part_one(input: String) -> i32 {
                 ".." => {
                     pwd.pop();
                 }
-                r"/" => pwd = vec!["/"],
+                "/" => cdir = dir,
                 _ => {
-                    pwd.push(dir);
-                    dirs.entry(dir).or_insert(0);
+                    pwd.push(cdir);
+                    dirs.entry(pwd.join("/")).or_default();
                 }
             };
+        } else if let Some(dir) = line.strip_prefix("dir ") {
+            cdir = dir;
         } else {
             if let Ok(size) = line.split_once(" ").unwrap().0.parse::<i32>() {
-                for f in &pwd {
-                    dirs.entry(f).and_modify(|e| *e += size);
+                for (i,_) in pwd.iter().enumerate(){
+                    dirs.entry(pwd[0..pwd.len()-i].join("/")).and_modify(|e|*e+=size);
                 }
             }
         }
     });
-    // dirs.into_values().fold(0, |acc, v| {
-        // dbg!(acc,v);
-        // acc + match v {
-            // i if i < 100_000 => i,
-            // _ => 0,
-        // }}
-    let test:Vec<i32>= dirs.into_values().collect();
-    test.iter().filter(|n| **n<=100_000).sum()
+    dirs.into_values()
+        .filter(|n| *n <= 100_000)
+        .fold(0, |acc, v| acc + v)
 }
 fn main() {
-    let _input = std::fs::read_to_string("inputs/day7_2.txt").unwrap();
-    println!("sum: {}", part_one(_input));
+    let input = std::fs::read_to_string("inputs/day7_2.txt").unwrap();
+    println!("sum: {}", part_one(input));
     println!("sum: {}", part_one(EXAMPLE_INPUT.to_string()));
 }
 #[test]
 fn part_one_test() {
     assert_eq!(95437, part_one(EXAMPLE_INPUT.to_string()));
-    assert_eq!(24933642, part_one(std::fs::read_to_string("inputs/day7_2.txt").unwrap()))
+    assert_eq!(
+        24933642,
+        part_one(std::fs::read_to_string("inputs/day7_2.txt").unwrap())
+    )
 }
